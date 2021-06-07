fc7e834a0e5c8756cea04a095d2dcfc3073b1761
MattlttaM <47653451+MattlttaM@users.noreply.github.com>
2021-06-07 16:29:15 -0500

    Add files via upload

--- 01-JS/core/intro.js
+++ 01-JS/core/intro.js
@@ -44,6 +44,29 @@ console.log(typeof First);
 //First, hold the age in a variable.
 //      We can create variables with three keywords
 //          let, const, and var
+/*          
+                JavaScript is loosely typed and uses dynamic type checking.
+            let => Capable of being reassigned.
+            const => Constant. It cannot be redefined.
+            var =>  */
+
+
+//Scoping
+/**
+ *      age is global scoped
+ *      a and b is function scoped
+ *      c is block scoped
+ *  
+ */
+function add(a,b){
+    if(a < 0){
+        var c = Math.abs(a);
+    }
+    return c+b;
+}
+
+console.log(add(1,2));
+
 const age = 12;
 
 //Next, we need to write logic for determining the age.
@@ -88,10 +111,38 @@ console.log(8 === '8'); //Compares the entire entity
 if(overAge){
     console.log("The person is of age to join the site.")
 } else {
-    console.log("The securite check is too good for this.")
+    console.log("The security check is too good for this.")
 }
 
+// if else if
+
+//Switch
+
+//Loops!
+
+for(let i = 0; i < 10; i++){
+    console.log(i + ' is not 10.');
+}
+
+const max = 836;
+let iteration = 3;
+while(iteration < max){
+    if(max % iteration == 0){
+        console.log('The first divisible number is: ' + iteration);
+        break;
+    }
+    iteration++;
+}
+do{
+    console.log('running');
+}while(false);
 
 
-
+/**
+ Naming Conventions
+ *      constants UPPERCASE_WITH_SCREAMING_SNAKE
+        functions camelCase
+        variables camelCase
+        classes Pascalcase
+ */
 
